c3afacfa704cf3554fb5c1756f8ac72c2054a479
nwbirnie <nwbirnie@gmail.com>
2013-02-11 19:37:49 +0000

    add commentary

--- rpca.py
+++ rpca.py
@@ -7,6 +7,10 @@ import time
 import os
 
 def robust_pca(M):
+    """ 
+    Decompose a matrix into low rank and sparse components.
+    Returns L,S the low rank and sparse components respectively
+    """
     L = numpy.zeros(M.shape)
     S = numpy.zeros(M.shape)
     Y = numpy.zeros(M.shape)
@@ -20,10 +24,22 @@ def robust_pca(M):
     return L,S
     
 def svd_shrink(X, tau):
+    """
+    Apply the shrinkage operator to the singular values obtained from the SVD of X.
+    The parameter tau is used as the scaling parameter to the shrink function.
+    Returns the matrix obtained by computing U * shrink(s) * V where 
+        U are the left singular vectors of X
+        V are the right singular vectors of X
+        s are the singular values as a diagonal matrix
+    """
     U,s,V = numpy.linalg.svd(X, full_matrices=False)
     return numpy.dot(U, numpy.dot(numpy.diag(shrink(s, tau)), V))
     
 def shrink(X, tau):
+    """
+    Apply the shrinkage operator the the elements of X.
+    Returns V such that V[i,j] = max(abs(X[i,j]) - tau,0).
+    """
     V = numpy.copy(X).reshape(X.size)
     for i in xrange(V.size):
         V[i] = math.copysign(max(abs(V[i]) - tau, 0), V[i])
@@ -32,6 +48,10 @@ def shrink(X, tau):
     return V.reshape(X.shape)
             
 def frobeniusNorm(X):
+    """
+    Evaluate the Frobenius norm of X
+    Returns sqrt(sum_i sum_j X[i,j] ^ 2)
+    """
     accum = 0
     V = numpy.reshape(X,X.size)
     for i in xrange(V.size):
@@ -39,9 +59,17 @@ def frobeniusNorm(X):
     return math.sqrt(accum)
 
 def L1Norm(X):
+    """
+    Evaluate the L1 norm of X
+    Returns the max over the sum of each column of X
+    """
     return max(numpy.sum(X,axis=0))
 
 def converged(M,L,S):
+    """
+    A simple test of convergence based on accuracy of matrix reconstruction
+    from sparse and low rank parts
+    """
     error = frobeniusNorm(M - L - S) / frobeniusNorm(M)
     print "error =", error
     return error <= 10e-6
